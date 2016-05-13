4176032f20d7390d89a72f96efdad4013ad6b288
ilyakrasnovsky <ilyakrasnovsky@gmail.com>
2016-05-12 22:11:34 -0400

    registering offenders seems to working in dbclient.py, need group test

--- database/dbclient.py
+++ database/dbclient.py
@@ -9,6 +9,7 @@ classes:
 
 from dbmgr import Dbmgr
 import getpass
+import time
 
 '''
 class: DbClient
@@ -23,21 +24,64 @@ attributes:
 initializer input:
 	dbmgr : instance of the Dbmgr class
 
+functions:
+	registerOffender()
+	changeOffender()
+	unregisterOffender()
+
 '''
 
 class DbClient():
 	def __init__(self, dbmgr):
 		self.dbmgr = dbmgr
 		myname = getpass.getuser()
+		self.registerOffender(myname)
 
+	'''
+    function: registerOffender()
+
+    description:
+        -Registers a Crass offender in the database with default
+        attributes (5,5,5,5)
+
+    inputs: 
+        name : A string name of the offender to be Registers
+
+    outputs:
+    	None
+    '''
 	def registerOffender(self, name):
-		pass
+		print ("Welcome to Crass! You're such an asshole."
+			   " Registering you as an offender ...")
+		newOffender = {
+			"name" : name,
+			"attr" : {
+		        'speed' : 5,
+		        'accuracy' : 5,
+		        'readability' : 5,
+		        'confidence' : 5 
+			}
+		}
+		status = self.dbmgr.addOffender(newOffender)
+		time.sleep(3)
+		if (status == False):
+			print ("Oh wait, it looks like you're fucking in here"
+				" already. Bullshit. Well, good for you, asshole.")
+		else:
+			print ("Done! Do you feel good about yourself?")
 
 	def changeOffender(self):
 		pass
 
 	def unregisterOffender(self):
 		pass
+
+#Tester client
+def main():
+	dbmgr = Dbmgr()
+	dbclient = DbClient(dbmgr)
+	#while(True):
+	#	welcome = input()
 
 if __name__ == '__main__':
     main()
